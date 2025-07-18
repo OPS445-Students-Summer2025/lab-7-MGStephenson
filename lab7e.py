5ce4c20a30138aeb6645a573d399149702902802
Mario Stephenson <mgstephenson@myseneca.ca>
2025-07-18 01:05:19 -0400

    Completed lab7, no check script on github.

--- lab7e.py
+++ lab7e.py
@@ -13,7 +13,17 @@ class Time:
         self.hour = hour
         self.minute = minute
         self.second = second
+   
+    def __str__(self):
+        """Return a user-friendly string version of the Time object"""
+        return self.format_time()
 
+    def __repr__(self):
+        '''return a string representation for the object self'''
+        '''just instead of ':', you are required use the '.'  in the formatting string.'''
+        return f'{self.hour:02d}.{self.minute:02d}.{self.second:02d}'
+
+     
     def format_time(self):
         """Return time object as a formatted string"""
         return f'{self.hour:02d}:{self.minute:02d}:{self.second:02d}'
--- lab7f.py
+++ lab7f.py
@@ -13,6 +13,19 @@ class Time:
         self.hour = hour
         self.minute = minute
         self.second = second
+   
+    def __str__(self):
+        """Return a user-friendly string version of the Time object"""
+        return self.format_time()
+
+    def __repr__(self):
+        '''return a string representation for the object self'''
+        '''just instead of ':', you are required use the '.'  in the formatting string.'''
+        return f'{self.hour:02d}.{self.minute:02d}.{self.second:02d}'
+
+    def __add__(self, t2):
+        """return the result by using sum_times() method"""
+        return self.sum_times(t2)
 
     def format_time(self):
         """Return time object as a formatted string"""
